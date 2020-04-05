e66aff06a57457e1688f1ab0a8da70cfad6b1e89
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-04-05 00:28:52 -0500

    check expression balance

---
+++ practice0/balance.py
@@ -0,0 +1,40 @@
+
+
+def checkBalance(expression):
+    stack = []
+
+    for i in range(len(expression)):        
+
+        exp = expression[i]
+
+        if(exp == '(' or exp == '[' or exp == '{'):
+            stack.append(exp)
+            continue
+
+        if(len(stack) == 0):
+            return False
+
+        if(exp == ')'):
+            tmp = stack.pop()
+            if(tmp != '('):
+                return False
+        elif(exp == ']'):
+            tmp = stack.pop()
+            if(tmp != '['):
+                return False
+        elif(exp == '}'):
+            tmp = stack.pop()
+            if(tmp != '{'):
+                return False
+
+    return len(stack) == 0
+
+if __name__ == "__main__":
+
+    while(True):
+        exp = input('expres: ')
+
+        if(exp == 'q'):
+            break
+
+        print('Result: {}\n'.format(checkBalance(exp)))
