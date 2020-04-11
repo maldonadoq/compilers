8c420b022a7a06f7062331d27f011dae1937eb01
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-04-11 11:28:55 -0500

    lexical analyzer update

--- practice1/lexical.py
+++ practice1/lexical.py
@@ -1,17 +1,27 @@
+# Percy Maldonado Quispe
 
+
+# Token Class [word, idx, type]
 class Token:
+    # Constructor
     def __init__(self, _word, _idx, _type):
         self.word = _word
         self.idx = _idx
         self.type = _type
 
+    # Override Print Function
     def __str__(self):
         return 'Token[{}]: pos = {}, type = {}'.format(self.word, self.idx, self.type)
 
+
+# Lexical Analyzer Class
 class LexicalAnalizer:
     def __init__(self):
         pass
 
+    # Check character by character if it is digit, and add in a  temporary word,
+    # he loop ends if no character of type digit is found.
+    # The token and the new idx are returned.
     def isNumber(self, line, idx):
 
         tword = line[idx]
@@ -25,6 +35,9 @@ class LexicalAnalizer:
 
         return token, tidx
 
+    # Check character by character if it is alalnum, and add in a  temporary word,
+    # he loop ends if no character of type isalnum is found.
+    # The token and the new idx are returned.
     def isVariable(self, line, idx):
 
         tword = line[idx]
@@ -38,6 +51,8 @@ class LexicalAnalizer:
 
         return token, tidx
 
+    # Run the Lexical Analyzer, check if the character is a
+    # Digit, Alphabet or Operation.
     def run(self, line):
         idx = 0
 
@@ -56,6 +71,7 @@ class LexicalAnalizer:
             else:
                 idx += 1
 
+    # Print all Tokens
     def printTokens(self):
         for token in self.tokens:
             print(token)
