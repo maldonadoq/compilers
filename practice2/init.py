0e02eb0f0686bd29fd0e73dd3f2dc6cdf818e3e0
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-04-23 23:29:40 -0500

    flex and regex

---
+++ practice2/init.py
@@ -0,0 +1,32 @@
+import re
+
+def isNumber(line):
+    p = re.compile('[0-9]+')
+
+    t = False
+    if(p.match(line)):
+        t = True
+
+    return t
+
+def allWords(line):
+    reg = '[a-z]+'
+
+    groups = re.findall(reg, line)
+
+    for group in groups:
+        print(group)
+
+
+if __name__ == "__main__":
+
+    while(True):
+        line = input('line: ')
+
+        if(line == 'q'):
+            break    
+        
+        # print(isNumber(line))
+        allWords(line)
+        
+        print()
--- practice2/ip-id.py
+++ practice2/ip-id.py
@@ -1,5 +1,11 @@
 import re
 
+def checkIP(line):
+    rg = re.compile('^((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])$')
+
+    return rg.search(line) != None
+
+
 def checkIdentifier(line):
     rg = re.compile('^[a-z|A-Z]+[0-9]*')
 
@@ -13,5 +19,6 @@ if __name__ == "__main__":
         if(line == 'q'):
             break    
         
-        print(checkIdentifier(line))
+        print(checkIP(line))
+        # print(checkIdentifier(line))
         print()
