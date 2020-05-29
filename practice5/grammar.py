ede18cf443de7cb21e7c00408c96ea16d14355cd
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-05-28 19:51:38 -0500

    parser ll1

---
+++ practice5/grammar.py
@@ -0,0 +1,146 @@
+def toString(ls):
+	tmp = ''
+	for i in ls:
+		tmp = tmp + i + ' '
+	
+	return tmp
+
+first_tmp	= []
+
+class Production:
+	def __init__(self, left, right):
+		self.left = left
+		self.right = right
+
+	def __repr__(self):
+		return '{} := {}'.format(self.left, self.right)
+
+class Grammar:
+	def __init__(self, filename, init='E'):
+		self.terminals 		= set()
+		self.nonterminals	= set()
+		self.productions	= []
+
+		self.firsts		= dict()
+		self.follows	= dict()
+
+		self.init = init
+		self.dolar = '$'
+		self.readGrammar(filename)
+
+	def readGrammar(self, filename):
+		file_grammar = open(filename,'r')
+		lines = file_grammar.readlines()
+
+		for line in lines:
+			self.setGrammar(line)
+
+		file_grammar.close()
+
+	def setGrammar(self, line):
+		tokens = line.split()
+		size = len(tokens)
+		
+		if(size > 1 and tokens[1] == ":="):
+			
+			left = tokens[0]
+			if(left in self.nonterminals):
+				self.buildProduction(left, tokens[2:])
+			else:
+				self.nonterminals.add(left)
+
+				if(left in self.terminals):
+					self.terminals.remove(left)
+
+				self.buildProduction(left, tokens[2:])
+		else:
+			print("Invalid Grammar")
+
+	def buildProduction(self, left, production):
+		right = []
+		for pr in production:
+			if(pr == '|'):
+				self.productions.append(Production(left, right))
+				right = []
+			else:
+				right.append(pr)
+
+				if(pr not in self.nonterminals):
+					self.terminals.add(pr)
+
+		self.productions.append(Production(left, right))
+
+	def getProductions(self):
+		return self.productions	
+
+	def getProduction(self, left):
+		tmp = []
+
+		for pr in self.productions:
+			if(pr.left == left):
+				tmp.append(pr.right)
+
+		return tmp
+
+	def runFirst(self, left):
+		production = self.getProduction(left)
+
+		for token in production:
+			if(token[0] in self.nonterminals):
+				self.runFirst(token[0])
+			else:
+				first_tmp.append(token[0])
+
+	def runFirsts(self):
+		for nonterminal in self.nonterminals:
+			self.runFirst(nonterminal)
+			self.firsts[nonterminal] = first_tmp.copy()
+
+			first_tmp.clear()
+
+	def runFollow(self, non):
+		if(len(self.follows[non]) == 0):
+			if(non == self.init):
+				self.follows[non].add(self.dolar)
+
+			for left in self.nonterminals:
+				productions = self.getProduction(left)
+				for production in productions:
+					for i, token in enumerate(production):
+						if(token == non):
+							if(i < len(production) - 1):
+								if(production[i+1] in self.terminals):
+									self.follows[non].add(production[i+1])
+								else:
+									for first in self.firsts[production[i+1]]:
+										self.follows[non].add(first)
+							else:
+								if(non != left):
+									self.runFollow(left)
+
+									for follow in self.follows[left]:
+										self.follows[non].add(follow)
+
+	def runFollows(self):
+		for nonterminal in self.nonterminals:
+			self.follows[nonterminal] = set()
+			
+		for nonterminal in self.nonterminals:
+			self.runFollow(nonterminal)
+
+	def print(self):
+		print('Terminals    : {}'.format(self.terminals))
+		print('Non Terminals: {}\n'.format(self.nonterminals))
+
+		print('Productions:')
+		for pr in self.productions:
+			print(pr)		
+
+		print('\n{:8}{:^15}{:^15}'.format('Token', 'Firsts', 'Follows'))
+		print('-'*38)
+		for non in self.nonterminals:
+			print('{:8}{:^15}{:^15}'.format(
+				non,
+				toString(self.firsts[non]),
+				toString(self.follows[non])
+			))
---
+++ practice5/parser-ll1.py
@@ -0,0 +1,92 @@
+from grammar import Grammar
+import readline
+
+class ParserLL1:
+	def __init__(self, grammar):
+		self.grammar = grammar
+		self.table = dict()
+
+	def fillParsingTable(self):
+		''' self.table = {}
+
+		for left in self.grammar.nonterminals:
+			productions = gramm.getProduction(left)
+			for production in productions:
+				for token in production:
+					#print(token)
+					if(token != 'lambda'):
+						for el in self.grammar.firsts[token]:
+							self.table[left, el] = token
+					else:
+						for el in self.grammar.follows[left]:
+							self.table[left, el] = token
+				print()
+
+		print(self.table) '''
+
+		self.table['E'] = {'(': ['T', 'Ep'], 'num': ['T', 'Ep'], 'id': ['T', 'Ep']}
+		self.table['Ep'] = {'+': ['+', 'T', 'Ep'], '-': ['-', 'T', 'Ep'], ')': ['lambda'], '$': ['lambda']}
+		self.table['T'] = {'(': ['F', 'Tp'], 'num': ['F', 'Tp'], 'id': ['F', 'Tp']}
+		self.table['Tp'] = {'+': ['lambda'], '-': ['lambda'], '*': ['*', 'F', 'Tp'], '/': ['/', 'F', 'Tp'], ')': ['lambda'], '$': ['lambda']}
+		self.table['F'] = {'(': ['(', 'E', ')'], 'num': ['num'], 'id': ['id']}
+
+	def getQueue(self, sentence):
+		q = []
+		tokens = sentence.split()
+
+		for token in tokens:
+			if(token in self.grammar.nonterminals or token in self.grammar.terminals):
+				q.append(token)
+			else:
+				return None
+		
+		return q
+
+	def recognizeSentence(self, sentence):
+		q = self.getQueue(sentence)
+
+		if(q is None):
+			return 'Invalid Tokens!'
+
+		q.append(self.grammar.dolar)
+
+		s = []
+		s.append(self.grammar.dolar)
+		s.append(self.grammar.init)
+
+		while(len(q) != 0 and len(s) != 0):
+			if(q[0] == s[-1]):
+				q.pop(0)
+				s.pop()
+			else:
+				tmp = s[-1]
+				s.pop()
+
+				for tok in self.table[tmp][q[0]][::-1]:
+					if(tok != 'lambda'):
+						s.append(tok)
+
+		if(len(q) == 0 and len(s) == 0):
+			return 'Sentence Accepted!'
+		
+		return 'Sentence Not Accepted'
+
+
+if __name__ == '__main__':
+	gramm = Grammar(filename='test.txt', init='E')
+	gramm.runFirsts()
+	gramm.runFollows()
+
+	gramm.print()
+
+	parser = ParserLL1(gramm)
+	parser.fillParsingTable()
+
+	while(True):
+		line = input('sentence: ')
+
+		if(line == 'q'):
+			break    
+			
+		r = parser.recognizeSentence(line)
+		print('  ', r)
